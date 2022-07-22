60e539283cab70afebb78abf0414aaa0ff53f592
YangZhao <452608532@qq.com>
2022-07-22 11:35:47 +0800

    7.22

--- ds_pipeline/get_dataset.py
+++ ds_pipeline/get_dataset.py
@@ -45,7 +45,7 @@ def get_dataset_pipeline():
     """
 
     if FLAGS.config.dataset.dataset_name == 'cifar10':
-        image_size = None
+        image_size = FLAGS.config.dataset.image_size
         ds = dataset_source.Cifar10(
             FLAGS.config.batch_size,
             FLAGS.config.dataset.image_level_augmentations,
@@ -53,7 +53,7 @@ def get_dataset_pipeline():
             image_size=image_size
         )
     elif FLAGS.config.dataset.dataset_name == 'cifar100':
-        image_size = None
+        image_size = FLAGS.config.dataset.image_size
         ds = dataset_source.Cifar100(
             FLAGS.config.batch_size, FLAGS.config.dataset.image_level_augmentations,
             FLAGS.config.dataset.batch_level_augmentations, image_size=image_size
@@ -71,8 +71,8 @@ def get_dataset_pipeline():
     if FLAGS.config.dataset.dataset_name == 'cifar10' or FLAGS.config.dataset.dataset_name == 'cifar100':
         if image_size is None:
             FLAGS.config.dataset.image_size = 32
-            FLAGS.config.dataset.num_channels = 3
-            FLAGS.config.dataset.num_classes = 10 if FLAGS.config.dataset.dataset_name == 'cifar10' else 100
+        FLAGS.config.dataset.num_channels = 3
+        FLAGS.config.dataset.num_classes = 10 if FLAGS.config.dataset.dataset_name == 'cifar10' else 100
     elif FLAGS.config.dataset.dataset_name == 'imagenet':   
         FLAGS.config.dataset.num_channels = 3
         FLAGS.config.dataset.num_classes = 1000
